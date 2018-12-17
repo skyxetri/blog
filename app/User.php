aebfccbdb8b165ba1461b5b793a91121b970b461
Sky xetri <nexgen.hero9205@gmail.com>
2018-12-17 09:11:00 +0545

    post model and controller

--- app/User.php
+++ app/User.php
@@ -27,4 +27,8 @@ class User extends Authenticatable  implements MustVerifyEmail
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function post()
+    {
+        return $this->hasMany('App/Post');
+    }
 }
